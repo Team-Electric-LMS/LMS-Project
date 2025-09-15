d173dac80ce4e4144fb08eb6b0598515b8f65dbb
ninaeriksson <nina_e74@hotmail.com>
2025-09-15 15:10:15 +0200

    Add new comment Nina

--- WebApplication1/NinasFile.cs
+++ WebApplication1/NinasFile.cs
@@ -1,5 +1,5 @@
 ﻿// this readonly int a is a 3
-public class NinasFile      //Ronya was here, Jasmin also
+public class NinasFile      //Ronya was here, Jasmin also, Nina
 {
     readonly int a = 3;
 }
