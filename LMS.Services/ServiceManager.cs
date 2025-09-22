d45a92e232cd32ed52d8997295c21b2fed48071a
ninaeriksson <nina_e74@hotmail.com>
2025-09-22 14:24:33 +0200

    sparar lokala ändringar innan merge

--- LMS.Services/ServiceManager.cs
+++ LMS.Services/ServiceManager.cs
@@ -5,10 +5,16 @@ namespace LMS.Services;
 public class ServiceManager : IServiceManager
 {
     private Lazy<IAuthService> authService;
+    private Lazy<IStudentService> studentService;
     public IAuthService AuthService => authService.Value;
+    public IStudentService StudentService => studentService.Value;
 
-    public ServiceManager(Lazy<IAuthService> authService)
+    public ServiceManager(
+        Lazy<IAuthService> authService,
+        Lazy<IStudentService> studentService
+    )
     {
         this.authService = authService;
+        this.studentService = studentService;
     }
 }
---
+++ LMS.Services/StudentService.cs
@@ -0,0 +1,47 @@
+﻿using LMS.Infractructure.Data;
+using LMS.Shared.DTOs;
+using Microsoft.EntityFrameworkCore;
+using Service.Contracts;
+
+namespace LMS.Services
+{
+    public class StudentService : IStudentService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<CourseDto?> GetCourseForStudentAsync(Guid studentId)
+        {
+            // Hämta studenten
+            var student = await _context.Users
+                .FirstOrDefaultAsync(s => s.Id == studentId.ToString());
+
+            if (student == null || student.CourseId == null)
+            {
+                return null;
+            }
+
+            // Hämta kursen
+            var courseDto = await _context.Courses
+                .Where(c => c.Id == student.CourseId)
+                .Select(c => new CourseDto
+                {
+                    Id = c.Id,
+                    CourseName = c.Name,
+                    Description = c.Description,
+                    StartDate = c.StartDate.ToDateTime(TimeOnly.MinValue),
+                    EndDate = c.EndDate.ToDateTime(TimeOnly.MinValue),
+                    TeacherName = c.Teachers
+                          .Select(t => t.FirstName + " " + t.LastName)
+                          .FirstOrDefault() ?? string.Empty
+                })
+                .FirstOrDefaultAsync();
+
+            return courseDto;
+        }
+    }
+}
