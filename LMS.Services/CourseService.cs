0d4e08813e3ecc09daa1e16e579499c6fd4fa873
Jasmin Dzanic <jasmin@dzanic.se>
2025-09-29 23:36:38 +0200

    local changes before merge

--- LMS.Services/CourseService.cs
+++ LMS.Services/CourseService.cs
@@ -1,19 +1,36 @@
+using AutoMapper;
+using Domain.Contracts.Repositories;
+using Domain.Models.Entities;
 using LMS.Shared.DTOs;
 using LMS.Shared.DTOs.CourseDTOs;
 using Service.Contracts;
-using Domain.Models.Entities;
-using Domain.Contracts.Repositories;
+using System.Transactions;
 
 namespace LMS.Services;
 // Service for managing course-related operations
 public class CourseService : ICourseService
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
 
-    public CourseService(IUnitOfWork unitOfWork)
+    public CourseService(IUnitOfWork unitOfWork, IMapper mapper)
     {
         _unitOfWork = unitOfWork;
+        _mapper = mapper;
     }
+
+    public async Task<CourseDto> GetCourseAsync(Guid id)
+    {
+        var course = await _unitOfWork.Courses.GetCourseByIdAsync(id);
+        return course == null ? throw new ArgumentException("Course not found") : _mapper.Map<CourseDto>(course);
+    }
+
+    public async Task<IEnumerable<CourseDto>> GetAllCoursesAsync()
+    {
+            var courses = await _unitOfWork.Courses.GetAllAsync();
+            return courses.Select(c => _mapper.Map<CourseDto>(c));
+    }
+
     // Fetch courses by teacher ID
     public async Task<IEnumerable<CourseDto>> GetCoursesByTeacherAsync(Guid teacherId)
     {
@@ -37,5 +54,20 @@ public class CourseService : ICourseService
             // Log exceptions
             throw new ApplicationException("An error occurred while fetching courses.", ex);
         }
+    }
+
+    public async Task<CourseDto> CreateCourseAsync(CreateCourseDto createCourseDto)
+    {
+        var course = _mapper.Map<Course>(createCourseDto);
+        _unitOfWork.Courses.Create(course);
+        await _unitOfWork.CompleteAsync();
+        return _mapper.Map<CourseDto>(course);
+    }
+    
+    public async Task UpdateCourseAsync(Guid id, UpdateCourseDto updateCourseDto)
+    {
+        var course = await _unitOfWork.Courses.GetCourseByIdAsync(id, trackChanges: true);
+        _mapper.Map(updateCourseDto, course);
+        await _unitOfWork.CompleteAsync();
     }
 }
--- Service.Contracts/ICourseService.cs
+++ Service.Contracts/ICourseService.cs
@@ -4,5 +4,9 @@ namespace Service.Contracts;
 // Service contract for course-related operations
 public interface ICourseService
 {
+    Task<IEnumerable<CourseDto>> GetAllCoursesAsync();
+    Task<CourseDto> GetCourseAsync(Guid id);
     Task<IEnumerable<CourseDto>> GetCoursesByTeacherAsync(Guid teacherId);
+    Task<CourseDto> CreateCourseAsync(CreateCourseDto createCourseDto);
+    Task UpdateCourseAsync(Guid id, UpdateCourseDto updateCourseDto);
 }
