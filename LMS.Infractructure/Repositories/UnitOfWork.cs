bc45608d0abecb93a0976d3f6d1ef649f1d88b8b
Tomas <tomasantonj@gmail.com>
2025-10-01 13:44:45 +0200

    changed the pattern for ModuleService, Modulerepo and hows its accessed through UOW

--- LMS.Infractructure/Repositories/UnitOfWork.cs
+++ LMS.Infractructure/Repositories/UnitOfWork.cs
@@ -8,11 +8,12 @@ public class UnitOfWork : IUnitOfWork
     private readonly Lazy<IUserRepository> userRepository;
     private readonly Lazy<ICourseRepository> courseRepository;
     private readonly Lazy<IStudentRepository> studentRepository;
-    private IModuleRepository? _modules;
-    public IModuleRepository Modules => _modules ??= new ModuleRepository(context);
+    private readonly Lazy<IModuleRepository> moduleRepository;
+
     public IUserRepository UserRepository => userRepository.Value;
     public ICourseRepository Courses => courseRepository.Value;
     public IStudentRepository Students => studentRepository.Value;
+    public IModuleRepository Modules => moduleRepository.Value;
 
     public UnitOfWork(ApplicationDbContext context, Lazy<IUserRepository> userRepository, Lazy<ICourseRepository> courseRepository,
         Lazy<IStudentRepository> studentRepository)
@@ -21,6 +22,7 @@ public class UnitOfWork : IUnitOfWork
         this.userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
         this.courseRepository = courseRepository ?? throw new ArgumentNullException(nameof(courseRepository));
         this.studentRepository = studentRepository ?? throw new ArgumentNullException(nameof(studentRepository));
+        this.moduleRepository = moduleRepository ?? throw new ArgumentNullException(nameof(moduleRepository));
     }
     public async Task CompleteAsync() => await context.SaveChangesAsync();
 }
